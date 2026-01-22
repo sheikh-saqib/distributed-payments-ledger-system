2ffb7c1ebc9dd93107063f3c50bf6a9a28c0495a
sheikhsaqibfayaz11-stack <sheikhsaqib40@gmail.com>
2026-01-22 13:04:53 +0000

    Changed from global locking to locking per account

--- internal/ledger/ledger.go
+++ internal/ledger/ledger.go
@@ -13,7 +13,8 @@ import (
 // It holds a reference to the storage layer and a mutex for concurrency control
 type Ledger struct {
 	store interfaces.LedgerStore // Interface to save ledger entries, can be any storage implementation
-	mu    sync.Mutex             // Mutex ensures thread-safe writes to prevent race conditions
+	muMap map[string]*sync.Mutex //stores the *sync.Mutex for each account in a map
+	mapMu sync.Mutex             // protects the muMap itself
 }
 
 // NewLedger is a constructor function that creates a new Ledger instance
@@ -21,7 +22,19 @@ type Ledger struct {
 func NewLedger(store interfaces.LedgerStore) *Ledger {
 	return &Ledger{
 		store: store, // Assign the storage implementation to the ledger's store field
+		muMap: make(map[string]*sync.Mutex),
 	}
+}
+
+func (l *Ledger) getAccountLock(accountId string) *sync.Mutex {
+
+	l.mapMu.Lock()
+	defer l.mapMu.Unlock()
+
+	if _, exists := l.muMap[accountId]; !exists {
+		l.muMap[accountId] = &sync.Mutex{}
+	}
+	return l.muMap[accountId]
 }
 
 // PostTransaction is the core method that processes a transaction
@@ -29,12 +42,21 @@ func NewLedger(store interfaces.LedgerStore) *Ledger {
 // ensuring double-entry accounting, and then saves them to the store
 func (l *Ledger) PostTransaction(ctx context.Context, tx models.Transaction) error {
 
-	// Acquire the mutex lock before entering the critical section
-	// This ensures that only one transaction is processed at a time
-	l.mu.Lock()
-	// defer ensures that the lock will be released when the function exits
-	// even if an error occurs or the function returns early
-	defer l.mu.Unlock()
+	//Get Locks for both accounts
+	debitMutex := l.getAccountLock(tx.FromAccount)
+	creditMutex := l.getAccountLock(tx.ToAccount)
+
+	// Lock in order to avoid deadlocks
+	if tx.FromAccount < tx.ToAccount {
+		debitMutex.Lock()
+		creditMutex.Lock()
+	} else {
+		creditMutex.Lock()
+		debitMutex.Lock()
+	}
+
+	defer debitMutex.Unlock()
+	defer creditMutex.Unlock()
 
 	// Basic validation: the transaction amount must be positive
 	if tx.Amount <= 0 {
